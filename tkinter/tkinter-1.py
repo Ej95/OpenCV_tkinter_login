e2db1f0093b52156715a7d6525d750fae8eca133
Chisato_so_good <ej38158136@gmail.com>
2024-04-05 23:06:40 +0800

    將 tkinter 換成 custom_tkinter 美化前端
    美化前端

--- tkinter/tkinter-1.py
+++ tkinter/tkinter-1.py
@@ -1,98 +1,64 @@
-import tkinter as tk
-import sys
-sys.path.append(r"openCV_py")
-sys.path.append(r"sql")
-import face
-import face_data
-import time
- 
-id = 0
+import tkinter
+import customtkinter
+from PIL import ImageTk,Image
+
+customtkinter.set_appearance_mode("System")  
+customtkinter.set_default_color_theme("green")  
 
 
-win = tk.Tk() #建立主視窗
-
-#標題
-win.title("tkinter test")
-
-#大小
-win.geometry() #寬 X 高
-
-win.resizable(0,0) # 1 = True, 0 = False
-
-#icon
-win.iconbitmap(r"C:\Users\ej381\GitHub\OpenCV_tkinter_login\tkinter\icon.ico") #ico
-
-#顏色
-win.config(background="skyblue")
-
-#透明度
-win.attributes("-alpha", 0.9) # 1~0 1 = 100% 0 = 0%
-
-#置頂
-win.attributes('-topmost', 0) # 1 = True, 0 = False
-
-#Image
-img = tk.PhotoImage(file=r"C:\Users\ej381\GitHub\OpenCV_tkinter_login\tkinter\button.png")
+app = customtkinter.CTk()  #建立 tkinter 視窗
+app.geometry("600x440")
+app.title('Login')
 
 
-def submit():
-    text_name = en_name.get()
 
-    text_student_id = str(en_student_id.get())
-
-    #判斷是否資料輸入正確
-    if text_name == face_data.get_name_by_student_id(text_student_id)[0][0] and text_student_id == face_data.get_student_id_by_student_name(text_name)[0][0]:
-        btn_face.config(text="人臉辨識",command=detect)
-        btn_login.config(text="輸入完成，請做臉部辨識",command=None)
-    else:
-        label_result.config(text="學號 或 名字 輸入錯誤", font=("微軟正黑體", 20))
-        label_result.grid(row=3,column=0,columnspan=2)
-
-
-def detect():
-    id = face.facedetect()
-
-    text_name = en_name.get()
-    text_student_id = str(en_student_id.get())
-
+def button_function():
+    app.destroy()            # 將視窗關閉
+    w = customtkinter.CTk()  
+    w.geometry("1280x720")
+    w.title('Welcome')
+    l1=customtkinter.CTkLabel(master=w, text="Home Page",font=('Century Gothic',60))
+    l1.place(relx=0.5, rely=0.5,  anchor=tkinter.CENTER)
+    w.mainloop()
     
-    if len(face_data.get_name_by_face_id(id)) == 0 and len(face_data.get_student_id_by_face_id(id)) == 0 and id != -1:
-        label_result.config(text="未偵測到臉，請在試一次", font=("微軟正黑體", 20))
-        label_result.grid(row=3,column=0,columnspan=2)
-    elif id != face_data.get_face_id_by_name(text_name)[0][0]:
-        label_result.config(text="臉部驗證失敗".format(text_name), font=("微軟正黑體", 20))
-        label_result.grid(row=3,column=0,columnspan=2)
-    elif text_name == face_data.get_name_by_face_id(id)[0][0] and text_student_id == face_data.get_student_id_by_face_id(id)[0][0]:
-        label_result.config(text="臉部驗證成功 歡迎 {}".format(text_name), font=("微軟正黑體", 20))
-        label_result.grid(row=3,column=0,columnspan=2)
 
 
-#Button
-btn_login = tk.Button(text="確認輸入", font=("微軟正黑體", 20), command= submit)
-btn_login.grid(row=2,column=1)
+img1=ImageTk.PhotoImage(Image.open("./assets/pattern.png"))
+l1=customtkinter.CTkLabel(master=app,image=img1)
+l1.pack()
 
-btn_face = tk.Button(text="人臉辨識(請先認證學號、姓名)", font=("微軟正黑體", 20))
-btn_face.grid(row=2,column=0)
+#creating custom frame
+frame=customtkinter.CTkFrame(master=l1, width=320, height=360, corner_radius=15)
+frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+l2=customtkinter.CTkLabel(master=frame, text="Log into your Account",font=('Century Gothic',20))
+l2.place(x=50, y=45)
 
+entry1=customtkinter.CTkEntry(master=frame, width=220, placeholder_text='Username')
+entry1.place(x=50, y=110)
 
+entry2=customtkinter.CTkEntry(master=frame, width=220, placeholder_text='Password', show="*")
+entry2.place(x=50, y=165)
 
-label_login_name = tk.Label(text="姓名",font=("微軟正黑體",20))
-label_login_name.grid(row=0,column=0)
+l3=customtkinter.CTkLabel(master=frame, text="Forget password?",font=('Century Gothic',12))
+l3.place(x=155,y=195)
 
-label_login_student_id = tk.Label(text="學號",font=("微軟正黑體",20))
-label_login_student_id.grid(row=1,column=0)
-
-label_result = tk.Label()
-
-#entry
-en_name = tk.Entry(width=40,font=("微軟正黑體",20))
-en_name.grid(row=0,column=1)
-
-en_student_id = tk.Entry(width=40,font=("微軟正黑體",20))
-en_student_id.grid(row=1,column=1)
-
+#Create custom button
+button1 = customtkinter.CTkButton(master=frame, width=220, text="Login", command=button_function, corner_radius=6)
+button1.place(x=50, y=240)
 
 
+img2=customtkinter.CTkImage(Image.open("./assets/Google__G__Logo.svg.webp").resize((20,20), Image.ANTIALIAS))
+img3=customtkinter.CTkImage(Image.open("./assets/124010.png").resize((20,20), Image.ANTIALIAS))
+button2= customtkinter.CTkButton(master=frame, image=img2, text="Google", width=100, height=20, compound="left", fg_color='white', text_color='black', hover_color='#AFAFAF')
+button2.place(x=50, y=290)
 
-win.mainloop() #常駐主視窗
+button3= customtkinter.CTkButton(master=frame, image=img3, text="Facebook", width=100, height=20, compound="left", fg_color='white', text_color='black', hover_color='#AFAFAF')
+button3.place(x=170, y=290)
+
+
+
+
+# You can easily integrate authentication system 
+
+app.mainloop()
